1b50330572c09d6c222988ab92cd4457958d5d8b
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2024-03-08 23:50:08 +0700

    fix: reformat script

--- Assets/Scripts/player/playerMovement.cs
+++ Assets/Scripts/player/playerMovement.cs
@@ -5,135 +5,115 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public CharacterController2D controller;
-    public GameObject FireAttack;
-    public GameObject FirePunch;
-    public GameObject FireBomb;
-    public Animator animator;
-    public Rigidbody2D rb;
-    private bool cantMove = false;
+    [SerializeField] public CharacterController2D controller;
+    [SerializeField] public Animator animator;
+    [SerializeField]  public Rigidbody2D rb;
     public float runSpeed = 40f;
     float horizontalMove = 0f;
-    public float delay = 2.0f;
-    private bool attackBlocked ;
     bool jump = false;
     bool crouch = false;
-    GameObject preSkill;
-    GameObject resSkill;
     // Update is called once per frame
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     void Update () {
+        //Normal moving and control Axis
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        
-        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
-
-        if(Input.GetButtonDown("Attack1")){
-            if(animator.GetFloat("velocity_y") == 0){
-                if(horizontalMove == 0.0f){
-                    Fight1();
-                }
-            }
-        }
-        if(Input.GetButtonDown("Attack2")){
-            Fight2();
-        }
-        if(Input.GetButtonDown("Attack3")){
-            Kick();
-        }
         if(Input.GetButtonDown("Jump")){
             jump = true;
         }
+
         if(Input.GetButtonDown("Crouch")){
             crouch = true;
         }else if(Input.GetButtonUp("Crouch")){
             crouch = false;
         }
+
+        //Control attack
+        if(Input.GetButtonDown("Attack2")){
+            //Fight2();
+        }
+        if(Input.GetButtonDown("Attack3")){
+           //Kick();
+        }
+        setAnimator();
     }
 
     void FixedUpdate () {
         // Move our characte
-        if(!cantMove){
-            controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-        }else{print("Khong di chuyen");}
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
-        Jump();
     }
 
-    public void Fight1(){
-        if (attackBlocked)
-        return;
-        animator.SetTrigger("attack_1");
-        cantMove = true;
-        attackBlocked = true;
-        StartCoroutine(DelayAttack_0(0.25f, FireAttack, 3.7f, -0.4f));
-        StartCoroutine(DelayAttack_1(1.1f, null));
-    }
-     public void Fight2(){
-        if (attackBlocked)
-        return;
-        animator.SetTrigger("attack_2");
-        attackBlocked = true;
-        StartCoroutine(DelayAttack_0(0.22f, FirePunch, -0.1f, -0.5f));
-        StartCoroutine(DelayAttack_1(1.0f, FireBomb));
-        StartCoroutine(DelayAttack_2(2f));
-    }
-    public void Kick(){
-        if (attackBlocked)
-        return;
-        animator.SetTrigger("attack_3");
-        attackBlocked = true;
-        StartCoroutine(DelayAttack());
-    }
-    public void Jump(){
-        float velocityY = rb.velocity.y;
-        animator.SetFloat("velocity_y", velocityY); 
-    }
-    private IEnumerator DelayAttack()
-    {
-        yield return new WaitForSeconds(0.5f);
-        attackBlocked = false;
-    }
-    private IEnumerator DelayAttack_0(float timDelay, GameObject skill, float xpos, float ypos)
-    {     
-        yield return new WaitForSeconds(timDelay);
-        Vector3 theScale = skill.transform.localScale;
-        
-        // Player is facing right
-        if(controller.isFaceRight){
-            if(theScale.x < 0){
-                theScale.x *= -1;
-                skill.transform.localScale = theScale;
-            }
-            preSkill = Instantiate(skill, new Vector3(transform.position.x + xpos, transform.position.y + ypos, 0f), transform.rotation);
-        }else{
-            if(theScale.x > 0){
-                theScale.x *= -1;
-                skill.transform.localScale = theScale;
-            }  
-            preSkill = Instantiate(skill, new Vector3(transform.position.x - xpos, transform.position.y + ypos, 0f), transform.rotation);
-        }
-    }
-    private IEnumerator DelayAttack_1(float timDelay, GameObject skill)
-    {
-        yield return new WaitForSeconds(timDelay);
-        if(skill != null){
-            resSkill = Instantiate(skill, new Vector3(preSkill.transform.position.x, preSkill.transform.position.y - 0.2f, 0f), transform.rotation);
+
+
+    private void setAnimator(){
+
+        //set isMoving
+        if(horizontalMove != 0.0f){
+            animator.SetBool("isMoving", true);
         }
         else{
-            attackBlocked = false;
+            animator.SetBool("isMoving", false);
         }
-        Destroy(preSkill);
-        cantMove = false;
-    }
-    private IEnumerator DelayAttack_2(float timDelay)
-    {
-        yield return new WaitForSeconds(timDelay);
-        Destroy(resSkill);
-        attackBlocked = false;
+
+        //set isJumping
+        float vy = rb.velocity.y;
+        if(vy >= 0.1f){
+            animator.SetBool("isJumping", true);
+        }else if(vy <= -0.1f){
+            animator.SetBool("isLanding", true);
+            animator.SetBool("isJumping", false);
+        }else{
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isLanding", false);
+        }
 
     }
+    // private IEnumerator DelayAttack()
+    // {
+    //     yield return new WaitForSeconds(0.5f);
+    //     attackBlocked = false;
+    // }
+    // private IEnumerator DelayAttack_0(float timDelay, GameObject skill, float xpos, float ypos)
+    // {     
+    //     yield return new WaitForSeconds(timDelay);
+    //     Vector3 theScale = skill.transform.localScale;
+        
+    //     // Player is facing right
+    //     if(controller.isFaceRight){
+    //         if(theScale.x < 0){
+    //             theScale.x *= -1;
+    //             skill.transform.localScale = theScale;
+    //         }
+    //         preSkill = Instantiate(skill, new Vector3(transform.position.x + xpos, transform.position.y + ypos, 0f), transform.rotation);
+    //     }else{
+    //         if(theScale.x > 0){
+    //             theScale.x *= -1;
+    //             skill.transform.localScale = theScale;
+    //         }  
+    //         preSkill = Instantiate(skill, new Vector3(transform.position.x - xpos, transform.position.y + ypos, 0f), transform.rotation);
+    //     }
+    // }
+    // private IEnumerator DelayAttack_1(float timDelay, GameObject skill)
+    // {
+    //     yield return new WaitForSeconds(timDelay);
+    //     if(skill != null){
+    //         resSkill = Instantiate(skill, new Vector3(preSkill.transform.position.x, preSkill.transform.position.y - 0.2f, 0f), transform.rotation);
+    //     }
+    //     else{
+    //         attackBlocked = false;
+    //     }
+    //     Destroy(preSkill);
+    //     cantMove = false;
+    // }
+    // private IEnumerator DelayAttack_2(float timDelay)
+    // {
+    //     yield return new WaitForSeconds(timDelay);
+    //     Destroy(resSkill);
+    //     attackBlocked = false;
+
+    // }
 }
 
