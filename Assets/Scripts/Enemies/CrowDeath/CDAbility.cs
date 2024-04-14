bdd66d6e5683c4e5183118c5ff1181bee4a82b0d
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2024-04-14 11:35:55 +0700

    feat: add enemy's HP [basic]

--- Assets/Scripts/Enemies/CrowDeath/CDAbility.cs
+++ Assets/Scripts/Enemies/CrowDeath/CDAbility.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrowDeathAbility : MonoBehaviour
+public class Ability : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int maxHP = 50;
+    [SerializeField] private int DPS = 2;
+    private AudioManager audioManager;
+    private Animator animator;
+    private Rigidbody2D rb;
+    void Awake()
     {
-        
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
     }
-
     // Update is called once per frame
-    void Update()
-    {
-        
+    public void TackDamage(){
+        if(maxHP > 1){
+            maxHP--;
+            animator.SetBool("isDamaged", false);
+            Debug.Log(maxHP);
+        }else{
+            Debug.Log(maxHP);
+            rb.velocity = new Vector2(0.0f,0.0f);
+            animator.SetBool("isDied", true);
+            audioManager.PlaySFX(audioManager.crowdeathDeath);
+        }
     }
 }
