24318cfd2c048ab053cc085c28dfedf71851fdd7
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2024-06-02 22:11:00 +0700

    chores

--- Assets/Scripts/Menu/MenuFunction.cs
+++ Assets/Scripts/Menu/MenuFunction.cs
@@ -21,33 +21,10 @@ public class MenuFunction : MonoBehaviour
             return;
         }
 
-        // Kiểm tra nếu GameManager.Instance không null
-        // if (GameManager.Instance == null)
-        // {
-        //     Debug.LogError("GameManager instance is not initialized.");
-        //     return;
-        // }
-
         // Lưu vị trí player vào GameManager
         GameManager.playerStartPosition = new Vector3(data.playerPosition[0], data.playerPosition[1], data.playerPosition[2]);
 
         // Tải cảnh mới
         SceneManager.LoadScene("Game");
     }
-
-    // private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    // {
-    //     if (scene.name == "Game")
-    //     {
-    //         // Đảm bảo chỉ đặt vị trí cho player khi cảnh "Game" được tải
-    //         Player player = FindObjectOfType<Player>();
-    //         if (player != null)
-    //         {
-    //             player.transform.position = GameManager.Instance.playerStartPosition;
-    //         }
-
-    //         // Hủy đăng ký sự kiện để không gây ra lỗi nhiều lần
-    //         SceneManager.sceneLoaded -= OnSceneLoaded;
-    //     }
-    // }
 }
