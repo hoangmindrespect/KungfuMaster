9267ccc2841f38c79ac10aaf5dc976bf26950688
ArthurNguyen1 <129584671+ArthurNguyen1@users.noreply.github.com>
2024-06-04 02:14:04 +0700

    feat: add timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    //bool timerActive = false;
+    float currentTime;
+    public int startMinutes;
+    public TextMeshProUGUI currentTimeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startMinutes * 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(timerActive == true)
+        //{
+        //    currentTime = currentTime - Time.deltaTime;
+        //}
+        currentTime = currentTime - Time.deltaTime;
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        //currentTimeText.text = "TIME: " + currentTime.ToString() + "s";
+        currentTimeText.text = "TIMER: " + time.Minutes.ToString() + ":" + time.Seconds.ToString();
+    }
+
+    //public void StartTimer()
+    //{
+    //    timerActive = true;
+    //}
+    
+    //public void StopTimer()
+    //{
+    //    timerActive = false;
+    //}
+}
