6c020125905a30cf8caaa691d1232d5cdc885a60
ArthurNguyen1 <ArthurNguyen0368@gmail.com>
2024-09-17 02:21:05 +0700

    feat: add item purchase function and demo

--- Assets/Scripts/Shop Manager/ShopManager.cs
+++ Assets/Scripts/Shop Manager/ShopManager.cs
@@ -6,19 +6,32 @@ using TMPro;
 
 public class ShopManager : MonoBehaviour
 {
+    public static ShopManager Instance { get; set; }
+
     public int coins;
     public TMP_Text coinUI;
     public ShopItemSO[] shopItemsSO;
     public ShopTemplate[] shopPanels;
     public Button[] myPurchaseBtns;
 
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
     void Start()
     {
         for (int i = 0; i < shopItemsSO.Length; i++)
             shopPanels[i].gameObject.SetActive(true);
         coinUI.text = "Coins: " + coins.ToString();
         LoadPanels();
-        //CheckPurchaseable();
+        CheckPurchaseable();
     }
 
     void Update()
@@ -30,30 +43,30 @@ public class ShopManager : MonoBehaviour
     {
         coins++;
         coinUI.text = "Coins: " + coins.ToString();
-        //CheckPurchaseable();
+        CheckPurchaseable();
     }
 
-    //public void CheckPurchaseable()
-    //{
-    //    for(int i = 0; i < shopItemsSO.Length; i++)
-    //    {
-    //        if (coins >= shopItemsSO[i].basePrice) // if I have enough money
-    //            myPurchaseBtns[i].interactable = true;
-    //        else
-    //            myPurchaseBtns[i].interactable = false;
-    //    }
-    //}
+    public void CheckPurchaseable()
+    {
+        for (int i = 0; i < shopItemsSO.Length; i++)
+        {
+            if (coins >= shopItemsSO[i].basePrice) // if I have enough money
+                myPurchaseBtns[i].interactable = true;
+            else
+                myPurchaseBtns[i].interactable = false;
+        }
+    }
 
-    //public void PurchaseItem(int btnNo)
-    //{
-    //    if (coins >= shopItemsSO[btnNo].basePrice) 
-    //    {
-    //        coins = coins - shopItemsSO[btnNo].basePrice;
-    //        coinUI.text = "Coins: " + coins.ToString();
-    //        CheckPurchaseable();
-    //        //Unlock item.
-    //    }
-    //}
+    public void PurchaseItem(int btnNo)
+    {
+        if (coins >= shopItemsSO[btnNo].basePrice)
+        {
+            coins = coins - shopItemsSO[btnNo].basePrice;
+            coinUI.text = "Coins: " + coins.ToString();
+            CheckPurchaseable();
+            //Unlock item.
+        }
+    }
 
     public void LoadPanels()
     {
