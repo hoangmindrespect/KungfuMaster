7e9757642cbb9a88bb4912bce77a8967e4e7d71d
ArthurNguyen1 <ArthurNguyen0368@gmail.com>
2024-09-16 18:54:52 +0700

    feat: add Scripts for Store and StoreItem, create Scriptable Object to store Item data

---
+++ Assets/Scripts/Shop Manager/ShopManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopManager : MonoBehaviour
+{
+    public int coins;
+    public TMP_Text coinUI;
+    public ShopItemSO[] shopItemsSO;
+    public ShopTemplate[] shopPanels;
+    public Button[] myPurchaseBtns;
+
+    void Start()
+    {
+        for (int i = 0; i < shopItemsSO.Length; i++)
+            shopPanels[i].gameObject.SetActive(true);
+        coinUI.text = "Coins: " + coins.ToString();
+        LoadPanels();
+        //CheckPurchaseable();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void AddCoins() // simple script to add coins
+    {
+        coins++;
+        coinUI.text = "Coins: " + coins.ToString();
+        //CheckPurchaseable();
+    }
+
+    //public void CheckPurchaseable()
+    //{
+    //    for(int i = 0; i < shopItemsSO.Length; i++)
+    //    {
+    //        if (coins >= shopItemsSO[i].basePrice) // if I have enough money
+    //            myPurchaseBtns[i].interactable = true;
+    //        else
+    //            myPurchaseBtns[i].interactable = false;
+    //    }
+    //}
+
+    //public void PurchaseItem(int btnNo)
+    //{
+    //    if (coins >= shopItemsSO[btnNo].basePrice) 
+    //    {
+    //        coins = coins - shopItemsSO[btnNo].basePrice;
+    //        coinUI.text = "Coins: " + coins.ToString();
+    //        CheckPurchaseable();
+    //        //Unlock item.
+    //    }
+    //}
+
+    public void LoadPanels()
+    {
+        for (int i = 0; i < shopItemsSO.Length; i++)
+        {
+            shopPanels[i].titleTxt.text = shopItemsSO[i].title;
+            shopPanels[i].priceTxt.text = "Coins: " + shopItemsSO[i].basePrice.ToString();
+        }
+    }
+}
