7c9f0c3279edbf6345cbc601d5c36c6df3574a0a
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2024-11-17 10:42:38 +0700

    feat: add poison spider (animation, animator, att)
    description: spider can attack every 10 seconds

---
+++ Assets/SpiderAttack.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAttack : MonoBehaviour
+{
+    private Animator animator;
+    private float spiderPositionX;
+    private float spiderPositionY;
+    private bool isFaceRight;
+
+    public GameObject player;
+    public GameObject poisonRightBullet;
+    public GameObject poisonLeftBullet;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spiderPositionX = this.gameObject.transform.position.x;
+        spiderPositionY = this.gameObject.transform.position.y;
+        isFaceRight = false;
+    }
+
+    public void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    void FixedUpdate()
+    {
+        if (player.transform.position.x < spiderPositionX)
+        {
+            if (isFaceRight)
+            {
+                isFaceRight = false;
+                Flip();
+            }
+        }
+        else
+        {
+            if (!isFaceRight)
+            {
+                isFaceRight = true;
+                Flip();
+            }
+        }
+        AnimatorStateInfo asi = animator.GetCurrentAnimatorStateInfo(0);
+        if (asi.IsName("attack"))
+        {
+            if (!isFaceRight)
+            {
+                Instantiate(poisonLeftBullet, new Vector3(spiderPositionX, spiderPositionY - 1.5f, 0.0f), Quaternion.identity);
+            }
+            else
+            {
+                Flip();
+                Instantiate(poisonRightBullet, new Vector3(spiderPositionX, spiderPositionY - 1.5f, 0.0f), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/SpiderLeftBulletMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderLeftBulletMovement : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector3(-10.0f, 0.0f, 0.0f);
+    }
+
+    void FixedUpdate()
+    {
+        AnimatorStateInfo asi = animator.GetCurrentAnimatorStateInfo(0);
+        if (asi.IsName("live-bullet"))
+        {
+            StartCoroutine(ResetIsAppearAfterDelay(5f));
+        }
+    }
+    private IEnumerator ResetIsAppearAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(this.gameObject);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(this.gameObject);
+    }
+}
